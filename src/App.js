a1c9f5c50b97b7f9f28d5e0a754016b332ff234f
danieldaga <daniel@arhrestauracion.com>
2023-01-25 23:43:08 +0100

    iteration 3 ??????

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 import ContactTable from './components/ContactTable/ContactTable';
 import contacts from "./contacts.json";
-
 
 const arrayContacts = contacts.splice(0, 5)
 
 const App = () => {
 
+
+
     return (
-        <ContactTable contacts={arrayContacts}/>
+        <div>
+            <ContactTable contacts={arrayContacts}/>
+        </div>
     )
 }
 
--- src/components/ContactTable/ContactTable.jsx
+++ src/components/ContactTable/ContactTable.jsx
@@ -1,46 +1,59 @@
+import { useState } from 'react'
 import './ContactTable.css'
-// import contacts from "../../contacts.json";
-
+import contact from "../../contacts.json";
 const ContactTable = (props) => {
 
     const {contacts} = props
+    // const [stateContacts, setStateContacts] = useState(contact)
+    // const firstFive = stateContacts.slice(0, 5)
+    // const addRandom = () => {
+    //     const randomIndex = Math.floor(Math.random() * stateContacts.length);
+    //     const randomContact = stateContacts[randomIndex];
+    //     firstFive.push(randomContact);
+    // };
+
 
     return(
+        <div>
+            <button className='random-contact'>Add Random Contact</button>
+            <button className='sort-populary'>Sort by populary</button>
+            <button className=''>Sort by name</button>
 
-        <table>
-            <thead>
-                <tr>
-                <th>Picture</th>
-                <th>Name</th>
-                <th>Popularity</th>
-                <th>Won Oscar</th>
-                <th>Won Emmy</th>
-                </tr>
-            </thead>
-            <tbody>
-                {contacts.map((contacts) => {
-                return (
-                    <tr key={contacts.id} className="table">
-                    <td>
-                        <img src={contacts.pictureUrl} alt={contacts.name} className='image-contact'/>
-                    </td>
-                    <td>{contacts.name}</td>
-                    <td>{contacts.popularity}</td>
-                        {
-                            contacts.wonOscar
-                            ? <td>🏆</td>
-                            : null
-                        }
-                        {
-                            contacts.wonEmmy
-                            ? <td>🏆</td>
-                            : null
-                        }
+            <table>
+                <thead>
+                    <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                    <th>Won Oscar</th>
+                    <th>Won Emmy</th>
                     </tr>
-                );
-                })}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {contacts.map((contacts) => {
+                    return (
+                        <tr key={contacts.id} className="table">
+                        <td>
+                            <img src={contacts.pictureUrl} alt={contacts.name} className='image-contact'/>
+                        </td>
+                        <td>{contacts.name}</td>
+                        <td>{contacts.popularity}</td>
+                            {
+                                contacts.wonOscar
+                                ? <td>🏆</td>
+                                : null
+                            }
+                            {
+                                contacts.wonEmmy
+                                ? <td>🏆</td>
+                                : null
+                            }
+                        </tr>
+                    );
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 export default ContactTable
