7bd56c0db2c61fc3fb49f89f5f7254b3687b63ad
danieldaga <daniel@arhrestauracion.com>
2023-01-26 20:01:27 +0100

    done

--- src/components/ContactTable/ContactTable.jsx
+++ src/components/ContactTable/ContactTable.jsx
@@ -50,8 +50,8 @@ const ContactTable = (props) => {
                     <th>Picture</th>
                     <th>Name</th>
                     <th>Popularity</th>
-                    <th>Won Oscar</th>
-                    <th>Won Emmy</th>
+                    <th>Won <br/> Oscar</th>
+                    <th>Won <br/> Emmy</th>
                     <th>Actions</th>
                     </tr>
                 </thead>
